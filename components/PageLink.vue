47a2aecb03b85a5f5fc19b5e9896e3423a00c1d0
ykob <info@tplh.net>
2021-02-07 22:32:38 +0900

    added page links on the home page.

---
+++ components/PageLink.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+NuxtLink.page(
+  :to = 'path'
+  )
+  |{{ page.title }}
+</template>
+
+<script>
+export default {
+  props: {
+    page: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    path() {
+      return this.page === null ? '' : `/model/${this.page.id}`
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,24 @@
 div
   h1
     |HOME
+  .pages
+    PageLink.page(
+      v-for = 'page in pages'
+      :page = 'page'
+      :key = 'page.id'
+      )
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    const pages = await $axios.get('/pages.json')
+
+    return {
+      pages: pages.data,
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
