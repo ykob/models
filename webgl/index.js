80251a53ef4e7005be97d7d1402cdc83220e103d
ykob <info@tplh.net>
2021-02-10 23:30:37 +0900

    change a kind of the light.

--- webgl/index.js
+++ webgl/index.js
@@ -11,7 +11,7 @@ export default class WebGLContent {
     this.floor = null
     this.controls = null
     this.glTFLoader = null
-    this.spotLight = null
+    this.directionalLight = null
     this.ambientLight = null
     this.models = Array(4)
     this.currentNum = 0
@@ -28,7 +28,7 @@ export default class WebGLContent {
     })
     this.renderer.setClearColor(0x000000, 0.0)
     this.renderer.shadowMap.enabled = true
-    this.renderer.shadowMap.type = THREE.BasicShadowMap
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
     await Promise.all([
       import('three/examples/jsm/controls/OrbitControls').then((module) => {
@@ -53,15 +53,22 @@ export default class WebGLContent {
     ])
 
     this.controls = new Controls(this.camera, this.renderer.domElement)
-    this.spotLight = new THREE.SpotLight('#cccccc', 2, 100, Math.PI / 4, 1)
+    this.directionalLight = new THREE.DirectionalLight('#cccccc', 0.5)
     this.ambientLight = new THREE.AmbientLight('#cccccc')
-    this.spotLight.position.set(0, 30, 20)
-    this.spotLight.castShadow = true
-    this.spotLight.shadow.mapSize.width = 2048
-    this.spotLight.shadow.mapSize.height = 2048
-    this.scene.add(this.spotLight)
+    this.directionalLight.position.set(0, 30, 30)
+    this.directionalLight.shadow.camera.top = 30
+    this.directionalLight.shadow.camera.right = 30
+    this.directionalLight.shadow.camera.bottom = -30
+    this.directionalLight.shadow.camera.left = -30
+    this.directionalLight.castShadow = true
+    this.directionalLight.shadow.mapSize.width = 2048
+    this.directionalLight.shadow.mapSize.height = 2048
+    this.scene.add(this.directionalLight)
     this.scene.add(this.ambientLight)
     this.resize(resolution)
+
+    // const helper = new THREE.CameraHelper(this.directionalLight.shadow.camera)
+    // this.scene.add(helper)
   }
 
   start() {
