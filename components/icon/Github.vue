a6dae467227e5677cf17b1588b8510801952cb55
yoichi kobayashi <info@tplh.net>
2021-02-24 02:46:12 +0900

    added github icon component.

---
+++ components/icon/Github.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+IconBase(
+  iconName = 'clock'
+  :size = 'size'
+  :fill = 'fill'
+  )
+  path(
+    :d = 'd'
+    )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mdiGithub } from '@mdi/js'
+
+export default Vue.extend({
+  props: {
+    size: {
+      type: [Number, String],
+      default: 24,
+    },
+    fill: {
+      type: String,
+      default: 'currentColor',
+    },
+  },
+  computed: {
+    d() {
+      return mdiGithub
+    },
+  },
+})
+</script>
