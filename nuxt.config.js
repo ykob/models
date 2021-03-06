cf873c76a61db60845b154375187c98c3e5bd23b
yoichi kobayashi <info@tplh.net>
2021-03-06 12:21:43 +0900

    disabled user scalable

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     title: 'models',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
