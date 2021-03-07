ef193a1d322cf014d08cdcc7c3ba4507dfa08c15
yoichi kobayashi <info@tplh.net>
2021-03-07 22:31:19 +0900

    update the scss branch of the model outline component.

--- components/model/ModelOutline.vue
+++ components/model/ModelOutline.vue
@@ -27,19 +27,55 @@ export default {
   display: flex;
   align-items: center;
   position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 5%;
+  .pc.portrait & {
+    bottom: 5%;
+    left: 5%;
+  }
+  .pc.landscape & {
+    top: 0;
+    bottom: 0;
+    left: 5%;
+  }
+  .mobile.portrait & {
+    bottom: 16px;
+    left: 16px;
+  }
+  .mobile.landscape & {
+    top: 0;
+    bottom: 0;
+    left: 16px;
+  }
   &__in {
     pointer-events: auto;
   }
   h1 {
     margin: 0;
-    font-size: 9vw;
+    .pc.portrait & {
+      font-size: 10.8vw;
+    }
+    .pc.landscape & {
+      font-size: 9vw;
+    }
+    .mobile.portrait & {
+      font-size: 14vw;
+    }
+    .mobile.landscape & {
+      font-size: 9vw;
+    }
   }
   .date {
-    font-size: 2vw;
-    text-transform: uppercase;
+    .pc.portrait & {
+      font-size: 3vw;
+    }
+    .pc.landscape & {
+      font-size: 2.5vw;
+    }
+    .mobile.portrait & {
+      font-size: 3.88vw;
+    }
+    .mobile.landscape & {
+      font-size: 2.5vw;
+    }
   }
 }
 </style>
