6651947e4d353b5b2379419093e08c25246237cd
yoichi kobayashi <info@tplh.net>
2021-03-15 23:49:07 +0900

    added error component.

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+  |{{ error.statusCode }}
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
